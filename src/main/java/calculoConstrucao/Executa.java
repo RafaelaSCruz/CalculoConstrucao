8f31835d46cd0d60207d0cf35d1e3c37e9e8a77b
Rafaela Cruz <rafaelacruz.s.2000@gmail.com>
2024-03-12 21:50:43 -0300

    Adaptação do sistema para diferentes classes.

---
+++ src/main/java/calculoConstrucao/Executa.java
@@ -0,0 +1,24 @@
+
+// 1-Pacote
+package calculoConstrucao;
+// 2-Referências às bibiliotecas
+
+import java.util.Scanner;
+
+// 3-Classes
+public class Executa {
+    // 3.1-Atributos
+    // 3.2-Métodos e Funções
+    static Scanner valor = new Scanner(System.in);
+    static String opcao;
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
+
+
+
+
