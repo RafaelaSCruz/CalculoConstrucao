90e1f5f1529e9f6a8d8d73f822eb380b72e04d95
Rafaela Cruz <rafaelacruz.s.2000@gmail.com>
2024-03-12 22:08:32 -0300

    Adaptação do sistema para diferentes classes.

---
+++ src/main/java/calculoConstrucao/Material.java
@@ -0,0 +1,17 @@
+package calculoConstrucao;
+
+public class Material extends Executa {
+
+    // Escolher peça (azulejo, madeira ou papel de parede)
+    public static void escolherMaterial() {
+        String material;
+        System.out.println("ESCOLHA O TIPO DO MATERIAL: \n" +
+                "1- Azulejo 15x15cm\n" +
+                "2- Madeira Laminada 130x20cm\n" +
+                "3- Papel de Parede 300x60cm\n" +
+                "COLOQUE O NÚMERO DA OPÇÃO DESEJADA: ");
+        material = valor.next();
+
+
+    }
+}
