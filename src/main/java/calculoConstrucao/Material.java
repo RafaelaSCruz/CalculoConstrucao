96f7f8e095f6b21f486fa3120bf4f73c1d85dd8f
Rafaela Cruz <rafaelacruz.s.2000@gmail.com>
2024-03-13 23:09:04 -0300

    Atualização no SistemaCompleto e inserção de 2 funções.

--- src/main/java/calculoConstrucao/Material.java
+++ src/main/java/calculoConstrucao/Material.java
@@ -12,6 +12,6 @@ public class Material extends Executa {
                 "COLOQUE O NÚMERO DA OPÇÃO DESEJADA: ");
         material = valor.next();
 
-//toDo: teste atualização github
+//toDo: refazer projeto com metodos e funções na mesma classe.
     }
 }
